dece7043270729f8e3ea26c14246095873bb6007
Thomas Lumley <tlumley@u.washington.edu>
2009-02-10 00:00:00 +0000

    version 2.35-3

--- R/format.Surv.s
+++ R/format.Surv.s
@@ -1,14 +1,16 @@
-# $Id: format.Surv.S 11059 2008-10-23 12:32:50Z therneau $
+# $Id: format.Surv.S 11273 2009-03-20 15:02:17Z tlumley $
 #
 format.Surv <- function(x, ...) format(as.character.Surv(x), ...)
 
 # The function to "make something suitable for inclusion in a data frame"
 #   was "as.data.frame.x" in versions <5, now it is "data.frameAux.x",
 #   so here we have a version specific definition.
-# This is only needed for Splus
+# This is needed for both S-plus and R
 if (!is.R()) {
     if (version$major >= 5) {
 	data.frameAux.Surv <- function(x, ...) data.frameAux.AsIs(x, ...)
 	} 
     else as.data.frame.Surv <- as.data.frame.model.matrix
-    }
+  } else {
+    as.data.frame.Surv <- as.data.frame.model.matrix
+  }
