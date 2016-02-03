76ee82204f711a6c0d21f5a1597d2794d24c88fb
Samphel Norden <snorden@ds04.nj01.303net.pvt>
2016-02-02 23:02:17 -0500

    AUD-2523 changes to create new wrapper method using .Call for coxdetail

---
+++ src/coxdetail_wrapper.c
@@ -0,0 +1,42 @@
+
+#include <R.h>
+#include <Rinternals.h>
+#include <Rdefines.h>
+#include "survS.h"
+#include "survproto.h"
+SEXP coxdetail_wrapper(SEXP   nusedx,   SEXP   nvarx,    SEXP   ndeadx,
+               SEXP y,        SEXP covar2,   SEXP   strata,
+               SEXP score,    SEXP weights,  SEXP means2,
+               SEXP u2,       SEXP var,      SEXP   rmat,
+               SEXP nrisk2,   SEXP work){
+
+        Sint *nusedx_int = INTEGER_POINTER(nusedx);
+        Sint *nvarx_int = INTEGER_POINTER(nvarx);
+        Sint *ndeadx_int = INTEGER_POINTER(ndeadx);
+        double *y_double = NUMERIC_POINTER(y);
+        double *covar2_double = NUMERIC_POINTER(covar2);
+        Sint *strata_int = INTEGER_POINTER(strata);
+        double *score_double = NUMERIC_POINTER(score);
+        double *weights_double = NUMERIC_POINTER(weights);
+        double *means2_double = NUMERIC_POINTER(means2);
+        double *u2_double = NUMERIC_POINTER(u2);
+        double *var_double = NUMERIC_POINTER(var);
+        double *rmat_double = NUMERIC_POINTER(rmat);
+        double *nrisk2_double = NUMERIC_POINTER(nrisk2);
+        double *work_double = NUMERIC_POINTER(work);
+
+        coxdetail(nusedx_int, nvarx_int, ndeadx_int, y_double,
+                  covar2_double, strata_int, score_double, weights_double,
+                  means2_double, u2_double, var_double,rmat_double, nrisk2_double,work_double);
+
+        int n = length(rmat_double);
+        SEXP out = PROTECT(allocVector(REALSXP, n));
+  
+        for (int i = 0; i < n; i++) {
+              REAL(out)[i] = REAL(rmat_double)[i] + 1;
+         }
+        UNPROTECT(1);
+
+        return out;
+        
+}
