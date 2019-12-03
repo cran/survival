7fda90018d50a6f1c0e7b689470109c50cb2a891
Terry M Therneau <therneau.terry@mayo.edu>
2019-12-03 16:30:02 +0000

    version 3.1-8

--- R/survfitCI.R
+++ R/survfitCI.R
@@ -267,7 +267,7 @@ survfitCI <- function(X, Y, weights, id, istate, cluster,
             }
         else {
             xx <- as.vector(unlist(lapply(clist, function(x) x[element])))
-            if (class(temp)=="table") matrix(xx, byrow=T, ncol=length(temp))
+            if (inherits(temp, "table")) matrix(xx, byrow=T, ncol=length(temp))
             else xx
         }
     }
@@ -319,8 +319,9 @@ survfitCI <- function(X, Y, weights, id, istate, cluster,
      
         if (influence) kfit$influence.pstate <- 
             lapply(curves, function(x) x$influence.pstate)
-        if (!missing(start.time)) kfit$start.time <- start.time
     }                         
+
+    if (!missing(start.time)) kfit$start.time <- start.time
     kfit$transitions <- mcheck$transitions
 
     #       
